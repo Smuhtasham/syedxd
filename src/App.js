f790d6c9f11f61fca179e79435dbae3ef3609add
Syed <muhtasham00@gmail.com>
2024-07-31 23:46:58 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
+// import Info from "./Components/Info";
 import Navbar from "./Components/Navbar";
 import Work from "./Components/Work";
 
@@ -13,6 +14,7 @@ function App() {
     <Hero/>
     <About/>
     <Work/>
+    {/* <Info/> */}
     <Footer/>
     </>
   );
