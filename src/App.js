2646b2f006ac8b8ecf1ccabfe977bd08fb32ea7b
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-03 17:33:12 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import About from "./Components/About";
+import Cross from "./Components/Cross";
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
-// import Info from "./Components/Info";
 import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
 import Work from "./Components/Work";
-
+import './index.css'
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
     <Hero/>
     <About/>
     <Work/>
-    {/* <Info/> */}
+    <Cross/>
+    <Projects/>
     <Footer/>
     </>
   );
---
+++ src/Components/Cross.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Cross() {
+  return (
+    <>
+      <div className="bg-[#11071F] py-10">
+        <div className="w-[90%] m-auto ">
+          <div className="justify-center text-center items-center flex flex-col  text-white">
+            <img className="w-[1100px]" src="skills.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
